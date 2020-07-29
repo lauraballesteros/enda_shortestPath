c5890483c251bd6274f57d913fec44fd2c7e0944
lauraballesteros <laura_nicollebt@hotmail.com>
2020-07-28 23:49:48 -0500

    added function checkTraffic that multiplies the cost between two nodes depending on the current traffic

--- readingFile.py
+++ readingFile.py
@@ -1,4 +1,13 @@
 import re
+
+def checkTraffic(cost,traffic):
+    if traffic == 'heavy\n':
+        cost = cost*1.2
+    elif traffic == 'light':
+        cost = cost*0.8
+    cost=int(cost)
+    print(cost)
+    return cost
 f = open("example.txt", "r")
 f=f.readlines()
 arr=[]
@@ -8,7 +17,7 @@ for line in f:
     else:
         (var1, var2, var3, var4) = line.split(",")
         var3=int(var3)
-        arr.append((var1,var2,var3,var4))
-print(var3)
+        cost=checkTraffic(var3,var4)
+        arr.append((var1,var2,cost))
 print(arr)    
     

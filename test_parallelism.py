859be50d3651ce9ac87be8e61e0d8b3ee3af0771
lauraballesteros <laura_nicollebt@hotmail.com>
2020-07-29 19:48:04 -0500

    trying pool to get the square of a series of numbers

--- test_parallelism.py
+++ test_parallelism.py
@@ -1,2 +1,16 @@
-import multiprocessing as mp
-print("Number of processors: ", mp.cpu_count())
+from multiprocessing import Pool
+
+def f(x):
+  return x**2
+  
+arr = [1,2,3,4,5]
+
+def main():
+    p = Pool()
+    res =  p.map(f, arr)
+    p.close()
+    p.join()
+    print(res)
+
+if __name__ == "__main__":
+    main()
